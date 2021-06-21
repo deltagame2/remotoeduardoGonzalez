47b04b2ca3ce6ec68ce974b9cd4caee5717a0e1d
edugo <edugo@DESKTOP-JVMPRIE>
2021-06-21 10:11:28 +0200

    rama de forma local

--- proyectoEduardoGonzalez/src/proyectoEduardoGonzalez/clase1.java
+++ proyectoEduardoGonzalez/src/proyectoEduardoGonzalez/clase1.java
@@ -1,5 +1,8 @@
 package proyectoEduardoGonzalez;
 
 public class clase1 {
-
+	public static void main(String[] args) {		
+		System.out.println("soy la linea 1 de la clase 1 creada por eduardo gonzalez ");
+		System.out.println("soy la linea 2 de la clase 1 creada por eduardo gonzalez ");
+	}
 }
---
+++ proyectoEduardoGonzalez/src/proyectoEduardoGonzalez/clase2.java
@@ -0,0 +1,7 @@
+package proyectoEduardoGonzalez;
+
+public class clase2 {
+	public static void main(String[] args) {		
+		System.out.println("soy la linea 1 de la clase2 y soy propiedad de  eduardo gonzalez ");
+		
+}
