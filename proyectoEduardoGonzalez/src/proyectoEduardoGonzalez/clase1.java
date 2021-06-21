1ec1f8236ccda578917e7a6613a16f3eb41b797f
edugo <edugo@DESKTOP-JVMPRIE>
2021-06-21 09:57:57 +0200

    forma remota

--- proyectoEduardoGonzalez/src/proyectoEduardoGonzalez/clase1.java
+++ proyectoEduardoGonzalez/src/proyectoEduardoGonzalez/clase1.java
@@ -1,5 +1,7 @@
 package proyectoEduardoGonzalez;
 
 public class clase1 {
-
+	public static void main(String[] args) {		
+		System.out.println("soy la linea 1 de la clase 1 creada por eduardo gonzalez");
+	}
 }
